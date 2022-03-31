959d9239b7eda0ca4644062e3c01ac7645e2e23b
rwietter <mauriciobw17@gmail.com>
2022-03-30 23:33:28 -0300

    edit

--- test.sh
+++ test.sh
@@ -1,11 +1,23 @@
 push_main () {
-  git add --all
-  git commit -m $1
-  git push origin main
+    git add --all
+    git commit -m $1
+    git push origin main
 }
 
-if [ $2 = "push_all" ]; then
+add_commit () {
+    git add --all
+    git commit -m $1
+}
+
+if [[ $op -eq 'push_all' ]]
+then
   push_main "$4"
+elif [[ $op -eq 'add_commit' ]]
+then
+  add_commit "$4"
+elif [[ $op -eq '--help' ]]
+then
+  echo "Usage: ./test.sh [push_all|add_commit] [commit_message]"
 else
   echo "Not a valid option"
 fi
