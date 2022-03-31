a818c84c46e65922baae2bc43fbcadfc05cc713a
Maurício Witter <46854467+rwietter@users.noreply.github.com>
2022-03-31 16:20:41 -0300

    Delete rust/hello_cargo/target directory

--- rust/hello_cargo/target/debug/deps/hello_cargo-a33be199dab9f171.d
+++
@@ -1,5 +0,0 @@
-/home/rwietter/github.com/rwietter/current_projects/notes/rust/hello_cargo/target/debug/deps/hello_cargo-a33be199dab9f171.rmeta: src/main.rs
-
-/home/rwietter/github.com/rwietter/current_projects/notes/rust/hello_cargo/target/debug/deps/hello_cargo-a33be199dab9f171.d: src/main.rs
-
-src/main.rs:
--- rust/hello_cargo/target/release/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/rwietter/github.com/rwietter/current_projects/notes/rust/hello_cargo/target/release/hello_cargo: /home/rwietter/github.com/rwietter/current_projects/notes/rust/hello_cargo/src/main.rs
